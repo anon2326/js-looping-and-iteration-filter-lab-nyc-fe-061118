74342a01e563c40d0ba651260687dca1316b00c2
Jeffrey Jiang <anon0503@gmail.com>
2018-07-30 22:41:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(drivers, string){
+var filteredDrivers = drivers.filter(function (driver) {
+    return driver.toLowerCase() === string.toLowerCase();
+});
+return filteredDrivers;
+}
+
+function fuzzyMatch(drivers, string){
+  var beginMatch;
+  return beginMatch;
+}
+
